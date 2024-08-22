b292871b1a4301012d2e3500ac3739bf3e5ccfaf
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-22 17:31:26 +0300

    добавление домашней страницы в urls

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from mailing.apps import MailingConfig
+from mailing.views import home
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('',)
+    path('', home, name='home')
 ]
