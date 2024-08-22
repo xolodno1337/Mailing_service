078267d5d0a68a44703b79c928d54d2cf7e8a752
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-22 15:50:03 +0300

    добавление class User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):  # Пользователь - тот, кто создает эти рассылки.
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email', help_text='Укажите ваш Email')
+    avatar = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', **NULLABLE,
+                               help_text='Загрузите ваш аватар')
+    phone = models.CharField(max_length=35, verbose_name='Номер телефона', **NULLABLE,
+                             help_text='Укажите ваш номер телефона')
+    token = models.CharField(max_length=100, verbose_name='Token', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
