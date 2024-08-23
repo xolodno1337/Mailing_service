871d39f33f29db3e59f24dbab4289b0f38881b7b
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-23 13:21:48 +0300

    добавление forms

---
+++ mailing/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm, BooleanField
+
+from mailing.models import Mailing
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for fild_name, fild in self.fields.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs['class'] = "form-check-input"
+            else:
+                fild.widget.attrs['class'] = "form-control"
+
+
+class MailingForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Mailing
+        fields = '__all__'
