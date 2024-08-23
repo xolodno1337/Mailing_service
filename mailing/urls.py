6a366b978514e3113bfc8f56705932f27b67fd62
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-23 12:13:29 +0300

    добавление CRUD для Mailing

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from mailing.apps import MailingConfig
-from mailing.views import home
+from mailing.views import home, MailingListView, MailingCreateView, MailingUpdateView, MailingDeleteView, \
+    MailingDetailView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('mailing/', MailingListView.as_view(), name='mailing_list'),
+    path('create', MailingCreateView.as_view(), name='mailing_create'),
+    path('update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
+    path('delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
+    path('mailing/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
 ]
