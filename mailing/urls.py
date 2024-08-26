ed34476c761a1424096b8ad9a4ab4366fa7350ad
Сергей Бутаков <xolodnotchk@mail.ru>
2024-08-26 12:29:22 +0300

    Добавлены urls для Message

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path
 from mailing.apps import MailingConfig
 from mailing.views import home, MailingListView, MailingCreateView, MailingUpdateView, MailingDeleteView, \
-    MailingDetailView
+    MailingDetailView, MessageListView, MessageCreateView, MessageUpdateView, MessageDeleteView, MessageDetailView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
+    # Рассылка:
     path('mailing/', MailingListView.as_view(), name='mailing_list'),
     path('create/', MailingCreateView.as_view(), name='mailing_create'),
     path('update/<int:pk>/', MailingUpdateView.as_view(), name='mailing_update'),
     path('delete/<int:pk>/', MailingDeleteView.as_view(), name='mailing_delete'),
     path('mailing/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
+    # Сообщения:
+    path('message/', MessageListView.as_view(), name='message_list'),
+    path('message/create/', MessageCreateView.as_view(), name='message_create'),
+    path('message/update/<int:pk>/', MessageUpdateView.as_view(), name='message_update'),
+    path('message/delete/<int:pk>/', MessageDeleteView.as_view(), name='message_delete'),
+    path('message/mailing/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
 ]
