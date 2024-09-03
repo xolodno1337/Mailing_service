5da28b5384c969065e93f068f76336b8c13c612e
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-03 16:38:51 +0300

    добавление 'login/'

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.html')),
+]
