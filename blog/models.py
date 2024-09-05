685c9580510f33f36d086e6c668dbead072b6417
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-05 13:21:23 +0300

    Добавление class Blog

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from users.models import User
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок блога', help_text='Укажите название блога')
+    body = models.TextField(verbose_name='Содержание статьи', help_text='Введите содержание статьи')
+    image_blog = models.ImageField(upload_to='blog/', verbose_name='Изображение', help_text='Загрузите изображение',
+                                   **NULLABLE)
+    views_count = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания', **NULLABLE)
+    owner = models.ForeignKey(User, verbose_name='Владелец', on_delete=models.SET_NULL, **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Блог'
+        verbose_name_plural = 'Блоги'
