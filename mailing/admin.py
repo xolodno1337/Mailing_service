e1dde2f66bd4973cc84834fa5dc56d0f31561c8b
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-05 16:50:39 +0300

    Добавление class MessageAdmin, class ClientAdmin

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,7 +1,17 @@
-from mailing.models import Mailing
+from mailing.models import Mailing, Message, Client
 from django.contrib import admin
 
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
     list_display = ('id', 'name_mailing', 'first_send_datetime', 'periodicity', 'status')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject_message', 'owner')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'comment', 'owner')
