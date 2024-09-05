4da6756a9f405bf5bfcba0d4a03271a3e63abd97
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-05 09:49:38 +0300

    Добавление для Create присвоение owner, добавление проверки на владельца и менеджера

--- mailing/views.py
+++ mailing/views.py
@@ -15,10 +15,23 @@ class MailingListView(LoginRequiredMixin, ListView):
     model = Mailing
     template_name = 'mailing_list.html'
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return Mailing.objects.all()
+        return Mailing.objects.filter(owner=user)
+
 
 class MailingDetailView(LoginRequiredMixin, DetailView):
     model = Mailing
     template_name = 'mailing/mailing_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if not (user.is_superuser or user.is_staff or obj.owner == user):
+            raise PermissionDenied('У вас нет прав для просмотра этого рассылки.')
+        return obj
 
 
 class MailingCreateView(LoginRequiredMixin, CreateView):
@@ -40,28 +53,54 @@ class MailingUpdateView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('mailing:mailing_list')
 
     def get_success_url(self):
-        return reverse('mailing:mailing_detail', args=[self.kwargs.get('pk')])
+        return reverse('mailing:mailing_detail', args=[self.object.pk])
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if self.request.user != obj.owner and not (self.request.user.is_superuser or self.request.user.is_staff):
+            raise PermissionDenied('У вас нет прав для редактирования этого объекта.')
+        return obj
 
     def get_form_class(self):
         user = self.request.user
         if user == self.object.owner:
             return MailingForm
-        if user.has_perm('mailing.can_view_mailing') and user.perm('mailing.can_block_mailing'):
+        if user.has_perm('mailing.can_view_mailing') and user.has_perm('mailing.can_mailing_is_active'):
             return MailingManagerForm
-        raise PermissionDenied
+        raise PermissionDenied('У вас нет прав для выполнения этого действия.')
 
 
 class MailingDeleteView(LoginRequiredMixin, DeleteView):
     model = Mailing
     success_url = reverse_lazy('mailing:mailing_list')
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if user != obj.owner and not user.is_superuser:
+            raise PermissionDenied('У вас нет прав для удаления этого объекта.')
+        return obj
+
 
 class MessageListView(LoginRequiredMixin, ListView):
     model = Message
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Message.objects.all()
+        return Message.objects.filter(owner=user)
+
 
 class MessageDetailView(LoginRequiredMixin, DetailView):
     model = Message
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if not (user.is_superuser or obj.owner == user):
+            raise PermissionDenied('У вас нет прав для просмотра этого рассылки.')
+        return obj
 
 
 class MessageUpdateView(LoginRequiredMixin, UpdateView):
@@ -69,24 +108,61 @@ class MessageUpdateView(LoginRequiredMixin, UpdateView):
     form_class = MessageForm
     success_url = reverse_lazy('mailing:message_list')
 
+    def get_success_url(self):
+        return reverse('mailing:message_detail', args=[self.object.pk])
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if user != obj.owner and not user.is_superuser:
+            raise PermissionDenied('У вас нет прав для редактирования этого объекта.')
+        return obj
+
 
 class MessageCreateView(LoginRequiredMixin, CreateView):
     model = Message
     form_class = MessageForm
     success_url = reverse_lazy('mailing:message_list')
 
+    def form_valid(self, form):
+        message = form.save()
+        user = self.request.user
+        message.owner = user
+        message.save()
+        return super().form_valid(form)
+
 
 class MessageDeleteView(LoginRequiredMixin, DeleteView):
     model = Message
     success_url = reverse_lazy('mailing:message_list')
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if user != obj.owner and not user.is_superuser:
+            raise PermissionDenied('У вас нет прав для удаления этого объекта.')
+        return obj
+
 
 class ClientListView(LoginRequiredMixin, ListView):
     model = Client
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return Client.objects.all()
+        return Client.objects.filter(owner=user)
+
 
 class ClientDetailView(LoginRequiredMixin, DetailView):
     model = Client
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if not (user.is_superuser or obj.owner == user):
+            raise PermissionDenied('У вас нет прав для просмотра этого рассылки.')
+        return obj
 
 
 class ClientUpdateView(LoginRequiredMixin, UpdateView):
@@ -94,16 +170,40 @@ class ClientUpdateView(LoginRequiredMixin, UpdateView):
     form_class = ClientForm
     success_url = reverse_lazy('mailing:client_list')
 
+    def get_success_url(self):
+        return reverse('mailing:client_detail', args=[self.object.pk])
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if user != obj.owner and not user.is_superuser:
+            raise PermissionDenied('У вас нет прав для редактирования этого объекта.')
+        return obj
+
 
 class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Client
     form_class = ClientForm
     success_url = reverse_lazy('mailing:client_list')
 
+    def form_valid(self, form):
+        client = form.save()
+        user = self.request.user
+        client.owner = user
+        client.save()
+        return super().form_valid(form)
+
 
 class ClientDeleteView(LoginRequiredMixin, DeleteView):
     model = Client
     success_url = reverse_lazy('mailing:client_list')
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        user = self.request.user
+        if user != obj.owner and not user.is_superuser:
+            raise PermissionDenied('У вас нет прав для удаления этого объекта.')
+        return obj
 
 
 def mailing_attempt_report(request):
