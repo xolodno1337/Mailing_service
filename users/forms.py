2386df8741746c64af9a1ec00d3f53934fa00efa
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-05 16:53:51 +0300

    Добавление class UserSuperForm, class UserForm

--- users/forms.py
+++ users/forms.py
@@ -20,7 +20,19 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
         fields = ('email', 'password1', 'password2',)
 
 
-class UserForm(ModelForm):
+class UserSuperForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class UserManagerForm(StyleFormMixin, ModelForm):
     class Meta:
         model = User
         fields = ('is_active',)
+
+
+class UserForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'avatar', 'phone', 'is_active']
