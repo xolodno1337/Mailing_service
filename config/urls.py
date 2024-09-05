03b1f96f317f2a22dc917265eda74036dfa2c15b
Сергей Бутаков <xolodnotchk@mail.ru>
2024-09-05 13:18:00 +0300

    Добавление приложения Blog + static

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailing.urls', namespace='mailing')),
-    path('users/', include('users.urls', namespace='users'))
-]
+    path('users/', include('users.urls', namespace='users')),
+    path('blog/', include('blog.urls', namespace='blog'))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
